08baf26db323d113bcc937e442e4ea41231d9522
sylinx_zj <sylinx_zj@163.com>
2020-12-30 16:46:12 +0900

    Initial commit

---
+++ src/main/java/com/sylinx/SpringSecurityDemo.java
@@ -0,0 +1,11 @@
+package com.sylinx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDemo {
+    public static void main(String []args) {
+        SpringApplication.run(SpringSecurityDemo.class, args);
+    }
+}
